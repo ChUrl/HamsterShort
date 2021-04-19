02a968ce155950fba730888c8c4c351e26c39102
churl <tobi@urpost.de>
2021-04-19 21:46:09 +0200

    initial commit

---
+++ src/main/kotlin/de/churl/hamstershort/HamsterShortApplication.kt
@@ -0,0 +1,11 @@
+package de.churl.hamstershort
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HamsterShortApplication
+
+fun main(args: Array<String>) {
+	runApplication<HamsterShortApplication>(*args)
+}
---
+++ src/test/kotlin/de/churl/hamstershort/HamsterShortApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.churl.hamstershort
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HamsterShortApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
